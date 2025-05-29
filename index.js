5c9e7b7eb71412241fcbbc31bb0f39a9dffdd14e
Mama`s soldier <159080981+Annaseer4002@users.noreply.github.com>
2025-05-29 10:50:55 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const orderRoutes = require('./routes/orders'); // Import order routes
+
+
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+
+
+// Routes
+app.use('/api/orders', orderRoutes); // Use the orders route
+
+const PORT = process.env.PORT || 8000;
+const MONGODB_URL = process.env.MONGODB_URL;
+
+mongoose
+  .connect(MONGODB_URL)
+  .then(() => {
+    console.log('Connected to MongoDB');
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  });
+
+
+
+
