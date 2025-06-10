f865f7cf6b1394b1ee17da96fdda15e43ec0f285
Mama`s soldier <159080981+Annaseer4002@users.noreply.github.com>
2025-06-10 14:39:08 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config();
-const cors = require('cors');
-app.use(cors());
+
 
 const orderRoutes = require('./routes/orders'); // Import order routes
 
@@ -18,7 +17,7 @@ app.use(express.json());
 // Routes
 app.use('/api/orders', orderRoutes); // Use the orders route
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 const MONGODB_URL = process.env.MONGODB_URL;
 
 mongoose
